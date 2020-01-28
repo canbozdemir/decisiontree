9dc066ac8a364ed48b1078eae4db4bd0dc0e02d2
Can Bozdemir <canbozdemir@hotmail.com.tr>
2020-01-28 02:15:51 +0100

    showed user's choices in the last screen

--- DecisionTree.Web/ClientApp/src/app/app.component.ts
+++ DecisionTree.Web/ClientApp/src/app/app.component.ts
@@ -15,21 +15,29 @@ export class AppComponent {
 
   private currentCondition: DecisionTreeCondition = <DecisionTreeCondition>{};
   private decisionTreeCondition: DecisionTreeCondition;
+  private selectedConditions: Array<any> = [];
 
 
   private getDecisionTreeCondition() {
     this.http.get<DecisionTreeCondition>('decisiontreecondition')
       .subscribe(response => {
         this.decisionTreeCondition = response;
-        this.changeCondition(this.decisionTreeCondition)
+        this.changeCondition(this.decisionTreeCondition, null);
       });
   }
 
-  private changeCondition(condition: DecisionTreeCondition) {
+  private changeCondition(condition: DecisionTreeCondition, answer: string) {
     this.currentCondition = condition;
+    this.saveUserSelections(condition.text, answer);
+  }
+
+  private saveUserSelections(conditionText: string, answer: string) {
+    this.selectedConditions.push({ conditionText, answer });
   }
 
   private redecide() {
     this.currentCondition = this.decisionTreeCondition;
+    this.selectedConditions = [];
+    this.changeCondition(this.decisionTreeCondition, null);
   }
 }
